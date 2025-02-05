73f0c98b6395cbeaef6c76e243a5021b7b7ece59
Fikshan <srjsuri44@gmail.com>
2025-02-05 15:16:33 +0530

    first code push

---
+++ tests/Google.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('C Open Google and check title', async ({ page }) => {
+  await page.goto('https://www.google.com');
+  await expect(page).toHaveTitle(/Google/);
+});
+
+test('D Open Facebook and check title', async ({ page }) => {
+    await page.goto('https://www.facebook.com');
+    await expect(page).toHaveTitle(/Facebook/);
+  });
+
+test('E Open Facebook and check title', async ({ page }) => {
+    await page.goto('https://www.gmail.com');
+    await expect(page).toHaveTitle(/Facebook/);
+  });
